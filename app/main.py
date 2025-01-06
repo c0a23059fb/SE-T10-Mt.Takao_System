c21ae367fbc853cb479ddcc9fe2c53e6ffa9c315
c0a23059fb <c0a23059fb@edu.teu.ac.jp>
2025-01-06 16:54:12 +0900

    アカウント間利用処理の追加及び、全体の動作確認完了

--- app/main.py
+++ app/main.py
@@ -7,20 +7,14 @@ from pydantic import BaseModel
 
 
 app = FastAPI()
-
-# MySQL 接続設定
-db_config = {
+db_config = { # MySQL 接続設定
     "host": "localhost",
     "port": 3306,
     "user": "root",
     "password": "password",
     "database": "main_db"
 }
-
-
-class Item(BaseModel):
-    name: str
-    description: str
+login_user = {"id": None, "name": "", "email": ""} # ログインユーザー識別用
 
 
 class Account(BaseModel):
@@ -44,27 +38,25 @@ def read_root():
         return HTMLResponse(content=f"Error: {e}", status_code=500)
 
 
-@app.post("/items/")
-def create_item(item: Item):
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO items (name, description) VALUES (%s, %s)", (item.name, item.description))
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return {"message": "Item created", "item": item}
-
-
 #アカウントの作成
 @app.post("/accounts/")
 def create_account(account: Account):
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO accounts (username, email, password) VALUES (%s, %s, %s)", (account.username, account.email, account.password))
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return {"message": "Account created", "account": account}
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO accounts (username, email, password) VALUES (%s, %s, %s)", \
+                    (account.username, account.email, account.password))
+        connection.commit() # DBに反映
+        cursor.execute("SELECT LAST_INSERT_ID()") # 登録したユーザーのIDを取得
+        login_user["id"] = cursor.fetchone()[0] # ログインユーザーID更新
+        cursor.close()
+        connection.close()
+        login_user["name"] = account.username # ログインユーザー名更新
+        login_user["email"] = account.email # ログインユーザーemail更新
+        return {"message": "Account created", "account": account}
+    except mysql.connector.Error as err: # 接続に失敗した場合
+        print(f"Error: {err}")
+        return {"message": "Account creation failed"}
 
 
 #QRコードの検証
@@ -74,36 +66,38 @@ def verify_qr(qr_code: QRCode):
     cursor = connection.cursor()
     cursor.execute("SELECT route_name, is_peak FROM stamps WHERE qr_code = %s", (qr_code.qr_code,))
     result = cursor.fetchone()
-    cursor.close()
-    connection.close()
+    
     if result:
         route_name, is_peak = result
         if is_peak:
-            return {"message": "QRコードが正しいです", "route_name": route_name, "is_peak": True}
+            # 盗聴スタンプを読み込んだら待機状態へ
+            cursor.execute("SELECT * FROM accounts WHERE id = %s", (login_user['id'],))
+            cursor.execute("UPDATE accounts SET state = %s WHERE id = %s", ("wait", login_user['id']))
         else:
-            return {"message": "QRコードが正しいです", "route_name": route_name, "is_peak": False}
+            # スタンプを読み込んだらアクティブ状態へ
+            cursor.execute("UPDATE accounts SET state = %s WHERE id = %s", ("active", login_user['id']))
+        connection.commit() # DBに反映
+        cursor.close()
+        connection.close()
+        return {"message": "QRコードは正常です", "route_name": route_name, "is_peak": bool(is_peak)}
     else:
+        cursor.close()
+        connection.close()
         return {"message": "QRコードが無効です"}
 
 
 # 特典の取得
-@app.post("/get_gift/")
-def get_gift(account: Account):
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor()
-    cursor.execute("SELECT COUNT(*) FROM stamps WHERE username = %s AND is_peak = True", (account.username,))
-    peak_count = cursor.fetchone()[0]
-    cursor.close()
-    connection.close()
-    print(peak_count)
+# @app.post("/get_gift/")
+# def get_gift(account: Account):
+#     connection = mysql.connector.connect(**db_config)
+#     cursor = connection.cursor()
+#     # cursor.execute("SELECT COUNT(*) FROM accounts WHERE username = %s AND is_peak = True", (account.username,))
+#     peak_count = cursor.fetchone()[0]
+#     cursor.close()
+#     connection.close()
+#     print(peak_count)
 
     # if peak_count >= 10:
     #     return {"message": "特典を獲得しました", "gift": "Special Gift"}
     # else:
     #     return {"message": "特典を獲得するには、10個のピークスタンプが必要です", "current_peak_count": peak_count}
-
-
-if __name__ == "__main__":
-    user_init = Account(username="test", email="test@test.te", password="test")
-    create_account(user_init)
-    get_gift(user_init)
--- db/init.sql
+++ db/init.sql
@@ -1,44 +1,31 @@
 CREATE DATABASE IF NOT EXISTS main_db;
 USE main_db;
 
-CREATE TABLE IF NOT EXISTS items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT
-);
-
 -- ユーザ-アカウント用のテーブル
 CREATE TABLE IF NOT EXISTS accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    state VARCHAR(10) DEFAULT 'passive' -- 無効：”passive”、有効”active”、待機”wait”
 );
 
 -- スタンプ用のテーブル
 CREATE TABLE IF NOT EXISTS stamps (
     id INT AUTO_INCREMENT PRIMARY KEY,
     route_name VARCHAR(255) NOT NULL,
-    qr_code VARCHAR(255) NOT NULL
+    qr_code VARCHAR(255) NOT NULL,
+    is_peak BOOLEAN DEFAULT FALSE
 );
-
-ALTER TABLE stamps ADD COLUMN is_peak BOOLEAN DEFAULT FALSE;
 
 INSERT INTO stamps (route_name, qr_code, is_peak) VALUES ('ルートA', 'QR_CODE_A1', FALSE);
 INSERT INTO stamps (route_name, qr_code, is_peak) VALUES ('ルートA', 'QR_CODE_A2', TRUE);
 INSERT INTO stamps (route_name, qr_code, is_peak) VALUES ('ルートB', 'QR_CODE_B1', FALSE);
 INSERT INTO stamps (route_name, qr_code, is_peak) VALUES ('ルートB', 'QR_CODE_B2', TRUE);
 
-
 -- 特典用のテーブル
 CREATE TABLE IF NOT EXISTS gifts (
-    id INT AUTO_DECREMENT PRIMATY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     store_name VARCHAR(255) NOT NULL,
     item_id INT NOT NULL
-)
-
-ALTER TABLE gifts ADD FOREIGN KEY (item_id) REFERENCES items(id);
-
-INSERT INTO items (name, description) VALUES ('蕎麦屋', '蕎麦10％引き');
-INSERT INTO items (name, description) VALUES ('団子屋', 'みたらし団子1個無料');
-INSERT INTO items (name, description) VALUES ('銭湯', 'レンタルタオル無料');
+);
